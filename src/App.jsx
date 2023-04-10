7a7884eff6117bcc0a99ac46b7f09342fd350f25
amalia <amalia@iii.org.tw>
2023-04-10 19:30:07 +0800

    mapping arrays to elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { Component } from "react";
-
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
@@ -9,34 +7,19 @@ class App extends Component {
 
     this.state = {
       // JSON object (data in Vue)
-      name: "Amal",
-      company: "Google",
-      bool: true,
+      mosnters: [
+        { name: "Frankenstein" },
+        { name: "Dracula" },
+        { name: "Zombie" },
+      ],
     };
-
-    // React will rerender when one state is compeletely changed to different object in memory (splice in Vue)
   }
-  changeName = () => {
-    let changed = this.state.bool ? { name: "Amalia" } : { name: "Amal" };
-    changed = { ...changed, bool: !this.state.bool };
-    this.setState(
-      () => {
-        return changed;
-      },
-      () => console.log(this.state)
-    );
-  };
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hi {this.state.name}, I work at {this.state.company}
-          </p>
-          <br />
-          <button onClick={this.changeName}>Change Name</button>
-        </header>
+        {this.state.mosnters.map((monster) => {
+          return <h1 key={monster.name}>{monster.name}</h1>;
+        })}
       </div>
     );
   }
